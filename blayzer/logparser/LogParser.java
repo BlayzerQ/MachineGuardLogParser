630fd3b2088c9aa39c632ffa2a17e4323a5496f0
Blayzer <oleg-doktor@mail.ru>
2016-06-22 15:46:26 +0300

    Upload files

---
+++ blayzer/logparser/LogParser.java
@@ -0,0 +1,19 @@
+package blayzer.logparser;
+
+public class LogParser {
+		
+	public void init() {
+		GUI.main(null);
+	}
+	
+	public void run() {
+
+	}
+	
+	public static void main (String[] args){
+        LogParser app = new LogParser();
+        
+        app.init();
+        app.run();
+	}
+}
